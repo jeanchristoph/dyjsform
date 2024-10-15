4ae224cba9fa2af7af6132b1f6c4a271601561dc
Jean-Christophe <malaval@topdata.fr>
2024-10-15 16:27:15 +0200

    petit debut

--- dyjsform.js
+++ dyjsform.js
@@ -1,65 +1,86 @@
+class Dyjsform {
+    constructor(entity = []) {
+        this.entity = entity;
+    }
+
+    getEntity(){
+        return this.entity;
+    }
+
+    setEntity(array){
+        this.entity = array;
+        return this;
+    }
+
+
 // Fonction pour générer le JSON
-function generateJson() {
-    var data = [];
-    document.querySelectorAll('#mod_topweb_adhesion_form .mod_topweb_adhesion_ligne').forEach(function(ligne) {
-        var typeAdhesion = ligne.querySelector('.mod_topweb_adhesion_type_adhesion').value;
-        var codeAdhesion = ligne.querySelector('.mod_topweb_adhesion_code_adhesion').value;
-        var value = ligne.querySelector('.mod_topweb_adhesion_value').value;
+    generateJson() {
+        var data = [];
+        document.querySelectorAll('#dysform .dysform_entity').forEach(function(ligne) {
+            var typeAdhesion = entity.querySelector('.dysform_type_adhesion').value;
+            var codeAdhesion = entity.querySelector('.dysform_code_adhesion').value;
+            var value = entity.querySelector('.dysform_value').value;
 
-        data.push({
-            "type_adhesion": typeAdhesion,
-            "code_adhesion": codeAdhesion,
-            "value": value
+            data.push({
+                "type_adhesion": typeAdhesion,
+                "code_adhesion": codeAdhesion,
+                "value": value
+            });
         });
-    });
 
-    document.querySelector('#mod_topweb_adhesion_adhesion_choices').value = JSON.stringify(data, null);
-}
+        document.querySelector('#dysform_options').value = JSON.stringify(data, null);
+    }
 
-// Fonction pour créer une ligne dans le formulaire
-function createLine(typeAdhesion = '', codeAdhesion = '', value = '') {
-    var newLigne = `
-        <div class="row form-group align-items-center mod_topweb_adhesion_ligne">
+    // Fonction pour créer une entity dans le formulaire Bootstrap 5
+    createEntity (typeAdhesion = '', codeAdhesion = '', value = '') {
+        var newLigne = `
+        <div class="row form-group align-items-center dysform_entity">
             <div class="form-group col-md-3">
                 <div class="col-md-12">Libellé</div>
                 <div class="col-md-12">
-                    <input class="form-control mod_topweb_adhesion_type_adhesion" type="text" value="${typeAdhesion}">
+                    <input class="form-control dysform_type_adhesion" type="text" value="${typeAdhesion}">
                 </div>
             </div>
             <div class="form-group col-md-3">
                 <div class="col-md-12">Code</div>
                 <div class="col-md-12">
-                    <input class="form-control mod_topweb_adhesion_code_adhesion" type="text" value="${codeAdhesion}">
+                    <input class="form-control dysform_code_adhesion" type="text" value="${codeAdhesion}">
                 </div>
             </div>
             <div class="form-group col-md-3">
                 <div class="col-md-12">Montant</div>
                 <div class="col-md-12">
-                    <input class="form-control mod_topweb_adhesion_value" type="text" value="${value}">
+                    <input class="form-control dysform_value" type="text" value="${value}">
                 </div>
             </div>
             <div class="form-group col-md-3">
                 <div class="col-md-12">&nbsp;</div>
                 <div class="col-md-12">
-                    <button type="button" class="remove_ligne btn btn-danger form-control">Supprimer</button>
+                    <button type="button" class="remove_entity btn btn-danger form-control">Supprimer</button>
                 </div>
             </div>
         </div>
     `;
-    document.querySelector('#mod_topweb_adhesion_form').insertAdjacentHTML('beforeend', newLigne);
-}
+        document.querySelector('#dysform').insertAdjacentHTML('beforeend', newLigne);
+    }
 
-// Charger le JSON à l'affichage de la page et générer les lignes
-function loadJson() {
-    var jsonString = document.querySelector('#mod_topweb_adhesion_adhesion_choices').value;
-    if (jsonString) {
-        try {
-            var jsonData = JSON.parse(jsonString);
-            jsonData.forEach(function(item) {
-                createLine(item.type_adhesion, item.code_adhesion, item.value);
-            });
-        } catch (error) {
-            console.error("Erreur lors de l'analyse du JSON : ", error);
+// Charger le JSON à l'affichage de la page et générer les entitys
+    loadJson() {
+        var jsonString = document.querySelector('#dysform_options').value;
+        if (jsonString) {
+            try {
+                var jsonData = JSON.parse(jsonString);
+                jsonData.forEach(function(item) {
+                    this.createEntity(item.type_adhesion, item.code_adhesion, item.value);
+                });
+            } catch (error) {
+                console.error("Erreur lors de l'analyse du JSON : ", error);
+            }
         }
     }
+
+
+
 }
+
+
