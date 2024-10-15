a2a8baf7d384419ff89d50c9706bdf801739293a
Jean-Christophe <64985971+jeanchristoph@users.noreply.github.com>
2024-10-15 18:55:24 +0200

    en cours

--- dyjsform.js
+++ dyjsform.js
@@ -1,6 +1,18 @@
 class Dyjsform {
-    constructor(entity = []) {
+
+    /**
+     *
+     * @param entity
+     * Exemple:  [{'html_element':'input','type': 'text', 'name': 'name'}]
+     * @param json
+     */
+    constructor(entity = [{'html_element':'input','type': 'text', 'name': 'name_1', 'value':''},
+        {'html_element':'input','type': 'text', 'name': 'name_2', 'value':''},
+        {'html_element':'input','type': 'text', 'name': 'name_3', 'value':''}
+    ], json = {}) {
         this.entity = entity;
+        this.json = json;
+        this.onFieldkeyup();
     }
 
     getEntity(){
@@ -9,59 +21,80 @@ class Dyjsform {
 
     setEntity(array){
         this.entity = array;
+        this.onFieldkeyup();
         return this;
     }
 
+    getJson(){
+        return this.json;
+    }
 
+    setJson(object){
+        this.json = object;
+        return this;
+    }
+
+    getFieldTemplate(field) {
+        const element =
+            `<div class="form-group col-md-3">
+            <div class="col-md-12">${field.name}</div>
+            <div class="col-md-12">
+                <${field.html_element} class="form-control ${field.name}" type="${field.type}" value="${field.value}">
+            </div>
+        </div>`;
+        return element;
+    }
+
+    onFieldkeyup () {
+        console.log('onFieldkeyup');
+        console.log(this.entity);
+        for (const entity of this.entity){
+            console.log(document.querySelector('.' + entity.name));
+            if (document.querySelector('.' + entity.name)){
+                document.querySelector('.' + entity.name).addEventListener('keyup', function() {
+                    this.generateJson();  // Générer le JSON sur keyup
+                });
+            }
+        }
+    }
+
+    handleEvent
 // Fonction pour générer le JSON
     generateJson() {
+        console.log('generateJson');
         var data = [];
-        document.querySelectorAll('#dysform .dysform_entity').forEach(function(ligne) {
-            var typeAdhesion = entity.querySelector('.dysform_type_adhesion').value;
-            var codeAdhesion = entity.querySelector('.dysform_code_adhesion').value;
-            var value = entity.querySelector('.dysform_value').value;
-
-            data.push({
-                "type_adhesion": typeAdhesion,
-                "code_adhesion": codeAdhesion,
-                "value": value
-            });
+        document.querySelectorAll('#dysform .dysform_entity').forEach(function(dysformEntity) {
+            let entityData = {};
+            for (let entity of this.entity) {
+                entityData[entity.name] = dysformEntity.querySelector('.' + entity.name).value;
+            }
+            data.push(entityData);
         });
 
         document.querySelector('#dysform_options').value = JSON.stringify(data, null);
     }
 
     // Fonction pour créer une entity dans le formulaire Bootstrap 5
-    createEntity (typeAdhesion = '', codeAdhesion = '', value = '') {
-        var newLigne = `
-        <div class="row form-group align-items-center dysform_entity">
-            <div class="form-group col-md-3">
-                <div class="col-md-12">Libellé</div>
-                <div class="col-md-12">
-                    <input class="form-control dysform_type_adhesion" type="text" value="${typeAdhesion}">
-                </div>
-            </div>
-            <div class="form-group col-md-3">
-                <div class="col-md-12">Code</div>
-                <div class="col-md-12">
-                    <input class="form-control dysform_code_adhesion" type="text" value="${codeAdhesion}">
-                </div>
-            </div>
-            <div class="form-group col-md-3">
-                <div class="col-md-12">Montant</div>
-                <div class="col-md-12">
-                    <input class="form-control dysform_value" type="text" value="${value}">
-                </div>
-            </div>
-            <div class="form-group col-md-3">
-                <div class="col-md-12">&nbsp;</div>
-                <div class="col-md-12">
-                    <button type="button" class="remove_entity btn btn-danger form-control">Supprimer</button>
-                </div>
-            </div>
+    createEntity () {
+        let begin =`<div class="row form-group align-items-center dysform_entity">`;
+        let end = `</div>`;
+        let deleteButton = `<div class="form-group col-md-3">
+        <div class="col-md-12">&nbsp;</div>
+        <div class="col-md-12">
+            <button type="button" class="remove_entity btn btn-danger form-control">Supprimer</button>
         </div>
-    `;
-        document.querySelector('#dysform').insertAdjacentHTML('beforeend', newLigne);
+    </div>`;
+        let HtmlForm = '';
+
+        HtmlForm = begin;
+        for (let entity of this.entity) {
+            console.log(this.entity);
+            console.log(entity);
+            HtmlForm = HtmlForm + this.getFieldTemplate(entity);
+        }
+        HtmlForm = HtmlForm + deleteButton;
+        HtmlForm = HtmlForm + end;
+        document.querySelector('#dysform').insertAdjacentHTML('beforeend', HtmlForm);
     }
 
 // Charger le JSON à l'affichage de la page et générer les entitys
--- exemple/exemple.js
+++ exemple/exemple.js
@@ -1,36 +1,35 @@
 // MODULE ADHESION
+document.addEventListener('DOMContentLoaded', function() {
+    let dyjsform = new Dyjsform();
+
 
 // Ajouter une nouvelle ligne
-document.getElementById('addEntity').addEventListener('click', function(event) {
-    event.preventDefault();
-    createLine();
-    resizeAccordeonContents();
-});
+    document.getElementById('add_entity').addEventListener('click', function(event) {
+        event.preventDefault();
+        dyjsform.createEntity();
+        resizeAccordeonContents();
+    });
 
 // Supprimer une ligne
-document.addEventListener('click', function(event) {
-    if (event.target && event.target.classList.contains('remove_entity')) {
-        event.preventDefault();
-        console.log('remove');
-        event.target.closest('.dysform_entity').remove();
-        generateJson();  // Régénérer le JSON après la suppression
-    }
-});
+    document.addEventListener('click', function(event) {
+        if (event.target && event.target.classList.contains('remove_entity')) {
+            event.preventDefault();
+            console.log('remove');
+            event.target.closest('.dysform_entity').remove();
+            dyjsform.generateJson();  // Régénérer le JSON après la suppression
+        }
+    });
 
 // Déclencher la génération du JSON sur chaque keyup
-document.querySelectorAll('.mod_topweb_adhesion_type_adhesion, .mod_topweb_adhesion_code_adhesion, .mod_topweb_adhesion_value').forEach(function(input) {
-    input.addEventListener('keyup', function() {
-        generateJson();  // Générer le JSON sur keyup
+    document.querySelectorAll('.mod_topweb_adhesion_type_adhesion, .mod_topweb_adhesion_code_adhesion, .mod_topweb_adhesion_value').forEach(function(input) {
+        input.addEventListener('keyup', function() {
+            dyjsform.generateJson();  // Générer le JSON sur keyup
+        });
     });
-});
 
 // Initialiser le JSON à l'ouverture de la page
-loadJson();  // Charger les données JSON au démarrage de la page
-generateJson();  // Générer le JSON initial
+    dyjsform.loadJson()// Charger les données JSON au démarrage de la page
+    dyjsform.generateJson(); // Générer le JSON initial
 
 // MODULE ADHESION FIN
-inputHide(document.getElementById('mod_topweb_montant_aff_btn_montant'), document.querySelector('.montant_btn_params'));
-
-document.getElementById('mod_topweb_montant_aff_btn_montant').addEventListener('change', function() {
-    inputHide(document.getElementById('mod_topweb_montant_aff_btn_montant'), document.querySelector('.montant_btn_params'));
 });
