847c74d69fb86982c4917c3be4d7f3bda790df30
Jean-Christophe <64985971+jeanchristoph@users.noreply.github.com>
2024-10-17 18:45:03 +0200

    création du json OK : a faire charger JSON pour affichage

--- exemple/exemple.js
+++ exemple/exemple.js
@@ -1,35 +1,13 @@
 // MODULE ADHESION
 document.addEventListener('DOMContentLoaded', function() {
     let dyjsform = new Dyjsform();
-
-// Ajouter une nouvelle ligne
-    document.getElementById('add_entity').addEventListener('click', function(event) {
-        event.preventDefault();
-        dyjsform.createEntity();
-        resizeAccordeonContents();
-    });
-
-// Supprimer une ligne
-    document.addEventListener('click', function(event) {
-        if (event.target && event.target.classList.contains('remove_entity')) {
-            event.preventDefault();
-            console.log('remove');
-            event.target.closest('.dysform_entity').remove();
-            dyjsform.generateJson();  // Régénérer le JSON après la suppression
-        }
-    });
-
-// Déclencher la génération du JSON sur chaque keyup
-    document.querySelectorAll('.mod_topweb_adhesion_type_adhesion, .mod_topweb_adhesion_code_adhesion, .mod_topweb_adhesion_value').forEach(function(input) {
-        input.addEventListener('keyup', function() {
-            dyjsform.generateJson();  // Générer le JSON sur keyup
-        });
-    });
-
-// Initialiser le JSON à l'ouverture de la page
-    dyjsform.loadJson()// Charger les données JSON au démarrage de la page
-    dyjsform.generateJson(); // Générer le JSON initial
-    dyjsform.loadEventListeners(); // Générer le JSON initial
-
+    const dyjsformEntities = [
+        {'html_element':'input','type': 'date', 'name': 'name_1', 'label': 'date','value':''},
+        {'html_element':'input','type': 'text', 'name': 'name_2', 'label': 'text','value':''},
+        {'html_element':'input','type': 'number', 'name': 'name_4', 'label': 'nombre','value':''},
+        {'html_element':'input','type': 'email', 'name': 'name_5', 'label': 'email','value':''},
+        {'html_element':'input','type': 'password', 'name': 'name_6', 'label': 'password', 'value':''},
+    ]
+    dyjsform.setEntity(dyjsformEntities);
 // MODULE ADHESION FIN
 });
