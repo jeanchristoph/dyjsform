0babf6241b8e836af3014df1525f7d00d9c997ce
Jean-Christophe <malaval@topdata.fr>
2024-10-22 18:56:22 +0200

    load json en cours

--- exemple/exemple.js
+++ exemple/exemple.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const dyjsformEntities = [
         {'html_element':'input','type': 'date', 'name': 'name_1', 'label': 'date','value':''},
         {'html_element':'input','type': 'text', 'name': 'name_2', 'label': 'text','value':''},
-        {'html_element':'input','type': 'number', 'name': 'name_4', 'label': 'nombre','value':''},
-        {'html_element':'input','type': 'email', 'name': 'name_5', 'label': 'email','value':''},
-        {'html_element':'input','type': 'password', 'name': 'name_6', 'label': 'password', 'value':''},
+        {'html_element':'input','type': 'number', 'name': 'name_3', 'label': 'nombre','value':''},
+        {'html_element':'input','type': 'email', 'name': 'name_4', 'label': 'email','value':''},
+        {'html_element':'input','type': 'password', 'name': 'name_5', 'label': 'password', 'value':''},
     ];
     dyjsform.setEntity(dyjsformEntities);
 // MODULE ADHESION FIN
