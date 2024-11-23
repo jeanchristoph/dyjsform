b95342c9b79d92d1ecf5048eb08b6d1ba3aa2f43
Jean-Christophe <64985971+jeanchristoph@users.noreply.github.com>
2024-11-23 23:46:23 +0100

    refacto getter setter avec proprieté privé et passage paramtre à l'utilisation

--- exemple/exemple.js
+++ exemple/exemple.js
@@ -3,16 +3,62 @@ import Dyjsform from '../dyjsform.js';
 // MODULE ADHESION
 document.addEventListener('DOMContentLoaded', function() {
     let dyjsform = new Dyjsform();
-    dyjsform.setTemplate('classic');
-    const dyjsformEntities = [
-        {'html_element':'input','type': 'date', 'name': 'name_date', 'label': 'date','value':'', 'content' : '', 'class' : ''},
-        {'html_element':'input','type': 'text', 'name': 'name_text', 'label': 'text','value':'', 'content' : '', 'class' : ''},
-        {'html_element':'input','type': 'number', 'name': 'name_number', 'label': 'nombre','value':'', 'content' : ''},
-        {'html_element':'input','type': 'email', 'name': 'name_email', 'label': 'email','value':'', 'content' : '', 'class' : ''},
-        {'html_element':'input','type': 'password', 'name': 'name_password', 'label': 'password', 'value':'', 'content' : '', 'class' : ''},
-        {'html_element':'button','type': 'button', 'name': 'djf_action_remove', 'label': '', 'value':'', 'content' : 'Supprimer', 'class' : 'btn btn-danger'},
+    dyjsform.template = 'classic';
+    dyjsform.entity = [
+        {
+            'html_element': 'input',
+            'type': 'date',
+            'name': 'name_date',
+            'label': 'date',
+            'value': '',
+            'content': '',
+            'class': ''
+        },
+        {
+            'html_element': 'input',
+            'type': 'text',
+            'name': 'name_text',
+            'label': 'text',
+            'value': '',
+            'content': '',
+            'class': ''
+        },
+        {
+            'html_element': 'input',
+            'type': 'number',
+            'name': 'name_number',
+            'label': 'nombre',
+            'value': '',
+            'content': ''
+        },
+        {
+            'html_element': 'input',
+            'type': 'email',
+            'name': 'name_email',
+            'label': 'email',
+            'value': '',
+            'content': '',
+            'class': ''
+        },
+        {
+            'html_element': 'input',
+            'type': 'password',
+            'name': 'name_password',
+            'label': 'password',
+            'value': '',
+            'content': '',
+            'class': ''
+        },
+        {
+            'html_element': 'button',
+            'type': 'button',
+            'name': 'djf_action_remove',
+            'label': '',
+            'value': '',
+            'content': 'Supprimer',
+            'class': 'btn btn-danger'
+        },
     ];
-    dyjsform.setEntity(dyjsformEntities);
     dyjsform.init();
 // MODULE ADHESION FIN
     // TODO laisser aussi la possibilité plus tard de faire juste un formaulaire banal sans ajout ni suppression avec un bouton valider.
