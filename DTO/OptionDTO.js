6e1a7e2aa19c9e6ba41249376ae473ec4ea75a7b
Jean-Christophe <64985971+jeanchristoph@users.noreply.github.com>
2024-12-11 16:41:47 +0100

    ajout du selecteur + DTO entity et option

---
+++ DTO/OptionDTO.js
@@ -0,0 +1,25 @@
+export default class OptionDTO {
+    /**
+     * Représente une option individuelle pour une entité.
+     * @param {string} name - Le nom affiché de l'option.
+     * @param {string} value - La valeur associée à l'option.
+     * @param {number|null} maxCount - Le nombre maximal (facultatif) d'utilisation de cette option.
+     */
+    constructor({name, value, maxCount = null}) {
+        this.name = name;
+        this.value = value;
+        this.maxCount = maxCount; // Nombre maximal (si applicable)
+    }
+
+    /**
+     * Convertit l'objet OptionDTO en un objet simple.
+     * @returns {Object} - Représentation JSON de l'option.
+     */
+    toJSON() {
+        return {
+            name: this.name,
+            value: this.value,
+            max_count: this.maxCount
+        };
+    }
+}
