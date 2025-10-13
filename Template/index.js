56d7145774f580ce8eab7f54fdfdc3202bd7f2d0
Jean-Christophe <64985971+jeanchristoph@users.noreply.github.com>
2025-10-13 18:57:42 +0200

    fix test tempalte

--- Template/index.js
+++ Template/index.js
@@ -4,10 +4,9 @@ async function importTemplate(name) {
     return import(`./${name}.js?v=${timestamp}`).then(module => module[name]);
 }
 
-const [Classic, Test, Bs5] = await Promise.all([
+const [Classic, Bs5] = await Promise.all([
     importTemplate('Classic'),
-    importTemplate('Test'),
     importTemplate('Bs5')
 ]);
 
-export { Classic, Test, Bs5 };
+export { Classic, Bs5 };
